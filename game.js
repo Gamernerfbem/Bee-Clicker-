9381afe26fbad4a9f5d4a7fabc86580a34c10683
Gamernerfbem <bjyanaga@gmail.com>
2026-04-16 20:12:20 -0700

    Refactor bee types and improve projectile logic

--- game.js
+++ game.js
@@ -13,9 +13,9 @@ const beeImages = {
   sword:"https://i.imgur.com/TiWHLiF.png",
   jouster:"https://i.imgur.com/lSAAT9W.png",
   samurai:"https://i.imgur.com/oxEFciK.png",
-  giant:"https://i.imgur.com/zdiy5OB.png", // 👈 MOVED POSITION
   lightsword:"https://i.imgur.com/MwAWDqF.png",
   king:"https://i.imgur.com/cp2Uw5i.png",
+  giant:"https://i.imgur.com/zdiy5OB.png",
 
   archer:"https://i.imgur.com/pN0vhfZ.png",
   ninja:"https://i.imgur.com/ghWPUE1.png",
@@ -29,30 +29,30 @@ const beeImages = {
 const projectiles = {
   archer:"https://i.imgur.com/aHbflNX.png",
   ninja:"https://i.imgur.com/Zayahc2.png",
-  wizard:"https://i.imgur.com/OmFtlCl.png",
+  wizard:"https://i.imgur.com/MeXbLdU.png",
   pistol:"https://i.imgur.com/ZSY3UQQ.png",
   rifle:"https://i.imgur.com/ZSY3UQQ.png",
   bazooka:"https://i.imgur.com/lbmO40Q.png",
-  queen:"hhttps://i.imgur.com/MeXbLdU.png"
+  queen:"https://i.imgur.com/DbDCX3O.png"
 };
 
-/* BEES (ORDER FIXED HERE) */
+/* BEES */
 const beeTypes = {
   stinger:{img:beeImages.stinger,power:1,speed:1300,cost:10,zone:"melee"},
   sword:{img:beeImages.sword,power:2,speed:1200,cost:25,zone:"melee"},
   jouster:{img:beeImages.jouster,power:5,speed:1100,cost:50,zone:"melee"},
-  samurai:{img:beeImages.samurai,power:7,speed:1000,cost:100,zone:"melee"},
-  giant:{img:beeImages.giant,power:10,speed:2500,cost:150,zone:"melee"}, // 👈 HERE
-  lightsword:{img:beeImages.lightsword,power:4,speed:800,cost:250,zone:"melee"},
-  king:{img:beeImages.king,power:20,speed:1500,cost:450,zone:"melee"},
+  samurai:{img:beeImages.samurai,power:7,speed:950,cost:100,zone:"melee"},
+  lightsword:{img:beeImages.lightsword,power:6,speed:700,cost:300,zone:"melee"},
+  king:{img:beeImages.king,power:20,speed:1700,cost:550,zone:"melee"},
+  giant:{img:beeImages.giant,power:15,speed:2000,cost:200,zone:"melee"},
 
-  archer:{img:beeImages.archer,projectile:"archer",power:2,speed:1300,cost:20,zone:"ranged"},
+  archer:{img:beeImages.archer,projectile:"archer",power:2,speed:1300,cost:25,zone:"ranged"},
   ninja:{img:beeImages.ninja,projectile:"ninja",power:3,speed:900,cost:75,zone:"ranged"},
-  wizard:{img:beeImages.wizard,projectile:"wizard",power:5,speed:1100,cost:150,zone:"ranged"},
-  pistol:{img:beeImages.pistol,projectile:"pistol",power:8,speed:800,cost:200,zone:"ranged"},
-  rifle:{img:beeImages.rifle,projectile:"rifle",power:5,speed:600,cost:250,zone:"ranged"},
+  wizard:{img:beeImages.wizard,projectile:"wizard",power:7,speed:1200,cost:100,zone:"ranged"},
+  pistol:{img:beeImages.pistol,projectile:"pistol",power:6,speed:800,cost:150,zone:"ranged"},
+  rifle:{img:beeImages.rifle,projectile:"rifle",power:5,speed:600,cost:200,zone:"ranged"},
   bazooka:{img:beeImages.bazooka,projectile:"bazooka",power:18,speed:2000,cost:350,zone:"ranged"},
-  queen:{img:beeImages.queen,projectile:"queen",power:25,speed:1500,cost:550,zone:"ranged"}
+  queen:{img:beeImages.queen,projectile:"queen",power:17,speed:1500,cost:550,zone:"ranged"}
 };
 
 /* CLICK */
@@ -72,30 +72,18 @@ function manualClick(){
 function showCrit(){
   const c = document.createElement("div");
   c.className = "critPopup";
-  c.innerText = "CRIT!";
+  c.innerText = "3x CRIT!";
   c.style.left = window.innerWidth/2 + "px";
   c.style.top = "120px";
   document.body.appendChild(c);
   setTimeout(()=>c.remove(),800);
 }
 
-function showCritAtBee(el){
-  const rect = el.getBoundingClientRect();
-
-  const c = document.createElement("div");
-  c.className = "critPopup";
-  c.innerText = "CRIT!";
-  c.style.left = rect.left + "px";
-  c.style.top = rect.top + "px";
-
-  document.body.appendChild(c);
-  setTimeout(()=>c.remove(),800);
-}
-
-/* SPAWN (HIGHER AREA FIX) */
+/* 🐝 SPAWN (FINAL FIXED SAFE ZONE) */
 function spawnBeeType(type){
   const d = beeTypes[type];
-  if(!d || honey < d.cost) return;
+  if(!d) return;
+  if(honey < d.cost) return;
 
   honey -= d.cost;
   d.cost = Math.floor(d.cost * 1.5);
@@ -109,21 +97,22 @@ function spawnBeeType(type){
   document.getElementById("beeArea").appendChild(bee);
 
   const area = document.getElementById("beeArea");
+
   const w = area.clientWidth;
   const h = area.clientHeight;
 
+  const topPad = h * 0.05;
+  const bottomPad = h * 0.25;
+
   let x, y;
 
-  const topPad = h * 0.05;
-  const usableHeight = h * 0.55;
-
-  if(d.zone === "melee"){
-    x = w * 0.45 + Math.random() * (w * 0.40);
+  if (d.zone === "melee") {
+    x = w * 0.45 + Math.random() * (w * 0.50);
+    y = topPad + Math.random() * (h - topPad - bottomPad);
   } else {
-    x = w * 0.05 + Math.random() * (w * 0.30);
+    x = w * 0.05 + Math.random() * (w * 0.35);
+    y = topPad + Math.random() * (h - topPad - bottomPad);
   }
-
-  y = topPad + Math.random() * usableHeight;
 
   bee.style.left = x + "px";
   bee.style.top = y + "px";
@@ -131,27 +120,19 @@ function spawnBeeType(type){
   beesArr.push({el:bee,data:d,cd:0});
 }
 
-/* LOOP (CRIT ADDED) */
+/* LOOP */
 setInterval(()=>{
   beesArr.forEach(b=>{
     b.cd -= 100;
 
     if(b.cd <= 0){
       if(!b.data.projectile){
+        const el = b.el;
+        el.classList.add("jump");
+        setTimeout(()=>el.classList.remove("jump"),120);
 
-        b.el.classList.add("jump");
-        setTimeout(()=>b.el.classList.remove("jump"),120);
-
-        let dmg = b.data.power;
-
-        if(Math.random() < critChance){
-          dmg *= 3;
-          showCritAtBee(b.el);
-        }
-
-        honey += dmg;
+        honey += b.data.power;
         updateUI();
-
       } else {
         fireProjectile(b.el,b.data);
       }
@@ -161,7 +142,7 @@ setInterval(()=>{
   });
 },100);
 
-/* PROJECTILES WITH CRIT */
+/* PROJECTILES */
 function fireProjectile(el,d){
   const p = document.createElement("img");
   p.className = "projectile";
@@ -186,15 +167,7 @@ function fireProjectile(el,d){
     if(Math.hypot(x-tx,y-ty)<25){
       p.remove();
       clearInterval(move);
-
-      let dmg = d.power;
-
-      if(Math.random() < critChance){
-        dmg *= 3;
-        showCritAtBee(el);
-      }
-
-      honey += dmg;
+      honey += d.power;
       updateUI();
     }
   },16);
@@ -231,7 +204,8 @@ function renderShop(){
 }
 
 function buyCritUpgrade(){
-  if(critLevel >= critMax || honey < critCost) return;
+  if(critLevel >= critMax) return;
+  if(honey < critCost) return;
 
   honey -= critCost;
   critLevel++;
